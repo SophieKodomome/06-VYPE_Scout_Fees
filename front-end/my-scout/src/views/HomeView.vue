96956c62b0b567a8f7ee4fc23961e7a03f8b1228
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-11-17 23:09:18 +0300

    update add fixed

--- front-end/my-scout/src/views/HomeView.vue
+++ front-end/my-scout/src/views/HomeView.vue
@@ -4,7 +4,15 @@ import { computed } from "@vue/reactivity";
 import { ref, onMounted } from "vue";
 
 // State variables
-const data = ref<{ people: any[] } | null>(null);
+const data =
+  ref<{
+    people: any[],
+    churchApprentices: any[],
+    churchLeaders: any[],
+    churchMembers: any[],
+    diosezeLeaders: any[],
+    districtLeaders: any[],
+  } | null>(null);
 const isLoading = ref(true);
 const error = ref<string | null>(null);
 const totalPaid = computed(() => {
@@ -46,43 +54,52 @@ console.log(data);
       </div>
 
       <!-- Data display -->
-      <div v-else class="flex flex-col space-y-4">
-        <section class="flex flex-row py-4 bg-blue-800 rounded-md">
-          <span class="w-14">
-            <h3 class="text-gray-100 font-semibold justify-self-center">ID</h3>
+      <div v-else>
+        <aside class="fixed top-2/4 w-96 h-80 right-8 bg-gray-100 px-8 pb-8 rounded-lg shadow-md overflow-scroll">
+          <h2 class=" text-red-600 uppercase font-semibold border-b-2 bg-gray-100 w-96 py-4">Attention</h2>
+            <span v-for="person in data?.people">
+            <p v-if="person.paid < person.due">{{person.name}}n'a pas encore pay&eacute;(e) sa cotisation</p>
           </span>
-          <span class="w-64">
-            <h3 class="text-gray-100 font-semibold">Name</h3>
-          </span>
-          <span class="w-48">
-            <h3 class="text-gray-100 font-semibold">Somme pay&eacute;</h3>
-          </span>
-          <span class="w-48">
-            <h3 class="text-gray-100 font-semibold">Somme demand&eacute;</h3>
-          </span>
-        </section>
-
-        <div class="bg-gray-300 flex flex-col rounded-md">
-          <section v-for="person in data.people" :key="person.id" class="flex flex-row py-4 bg-gray-200 rounded-md">
+        </aside>
+        <div class="flex flex-col space-y-4">
+          <section class="flex flex-row py-4 bg-blue-800 rounded-md">
             <span class="w-14">
-              <h3 class="text-gray-800 justify-self-center">{{ person.id }}</h3>
+              <h3 class="text-gray-100 font-semibold justify-self-center">ID</h3>
             </span>
             <span class="w-64">
-              <h3 class="text-gray-800">{{ person.name }}</h3>
+              <h3 class="text-gray-100 font-semibold">Name</h3>
             </span>
             <span class="w-48">
-              <h3 class="text-gray-800">{{ person.paid }}</h3>
+              <h3 class="text-gray-100 font-semibold">Somme pay&eacute;</h3>
             </span>
             <span class="w-48">
-              <h3 class="text-gray-800">{{ person.due }}</h3>
+              <h3 class="text-gray-100 font-semibold">Somme demand&eacute;</h3>
             </span>
           </section>
-        </div>
-        <div class="flex flex-row py-4 bg-green-200 rounded-md mt-4">
-          <span class="w-14"></span>
-          <span class="w-64 font-semibold">Totals:</span>
-          <span class="w-48 font-semibold">{{ totalPaid }}</span>
-          <span class="w-48 font-semibold">{{ totalDue }}</span>
+
+          <div class="bg-gray-300 flex flex-col rounded-md">
+            <section v-for="person in data.people" :key="person.id" class="flex flex-row py-4 bg-gray-200 rounded-md">
+              <span class="w-14">
+                <h3 class="text-gray-800 justify-self-center">{{ person.id }}</h3>
+              </span>
+              <span class="w-64">
+                <h3 class="text-gray-800">{{ person.name }}</h3>
+              </span>
+              <span class="w-48">
+                <h3 v-if="person.paid < person.due" class="text-red-800">{{ person.paid }}</h3>
+                <h3 v-else class="text-green-600">{{ person.paid }}</h3>
+              </span>
+              <span class="w-48">
+                <h3 class="text-gray-800">{{ person.due }}</h3>
+              </span>
+            </section>
+          </div>
+          <div class="flex flex-row py-4 bg-green-200 rounded-md mt-4">
+            <span class="w-14"></span>
+            <span class="w-64 font-semibold">Totals:</span>
+            <span v-if="totalPaid < totalDue" class="w-48 font-semibold">{{ totalPaid }}</span>
+            <span class="w-48 font-semibold">{{ totalDue }}</span>
+          </div>
         </div>
       </div>
     </div>
