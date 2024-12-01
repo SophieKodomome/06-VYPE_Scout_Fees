1b3156b598a0c5b18633bcba2ca01c5da2b25e3c
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-12-01 11:14:07 +0300

    Update view.sql

--- back-end/script/view.sql
+++ back-end/script/view.sql
@@ -1,14 +1,17 @@
 CREATE VIEW v_person_details AS
 SELECT 
-    p.id AS person_id,
-    r.role AS role_name,
+    p.id AS id,
+    p.id_String AS id_string,
+    r.id AS id_role,
+    r.role AS role,
     p.church AS church,
     p.district AS district,
     p.dioseze AS dioseze,
     r.subscription AS subscription,
     p.payment AS payment,
     p.year AS year,
-    p.birth_date AS birth_date
+    p.birth_date AS birth_date,
+    r.age_limitation AS age_limitation
 FROM 
     person p
 LEFT JOIN role r ON p.id_role = r.id;
