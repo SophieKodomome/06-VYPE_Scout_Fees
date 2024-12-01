33c2d31cc31a0452d17d399fa52fd753e359327d
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-12-01 11:30:17 +0300

    Update index.js

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -19,16 +19,10 @@ app.get("/getData", async (request, response) => {
     const prisma = new client_1.PrismaClient();
     try {
         const people = await prisma.v_person_details.findMany({
-            orderBy: { person_id: "asc" },
+            orderBy: { id: "asc" },
         });
-        const diosezes = await prisma.dioseze.findMany();
-        const districts = await prisma.district.findMany();
-        const churches = await prisma.church.findMany();
         const data = {
             people,
-            diosezes,
-            districts,
-            churches,
         };
         console.log("get Data");
         response.status(200).send({
